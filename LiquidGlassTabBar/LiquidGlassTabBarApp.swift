c5a54296df800c2f8ae6c8b2e8a9af373477bb6a
Tilak Shakya <shakyatilak45@gmail.com>
2025-06-11 10:02:53 +0530

    Initial Commit

---
+++ LiquidGlassTabBar/LiquidGlassTabBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  LiquidGlassTabBarApp.swift
+//  LiquidGlassTabBar
+//
+//  Created by Tilak Shakya on 11/06/25.
+//
+
+import SwiftUI
+
+@main
+struct LiquidGlassTabBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
